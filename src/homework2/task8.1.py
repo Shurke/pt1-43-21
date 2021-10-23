03dde68c2f3b66d91924c384a3efddf37d396542
Miron Chuduk <miron@chuduk.by>
2021-10-23 22:37:47 +0300

    Task 8.1
    Мини-калькулятор

---
+++ src/homework2/task8.1.py
@@ -0,0 +1,10 @@
+# The provided code stub reads two integers from STDIN, and.
+# Add code to print three lines where:
+# 1. The first line contains the sum of the two numbers.
+# 2. The second line contains the difference of the two numbers (first - second).
+# 3. The third line contains the product of the two numbers.
+a = int(input("Введите первое число: "))
+b = int(input("Введите второе число: "))
+print(f"Сумма двух чисел = {a + b}")
+print(f"Разность двух чисел = {a - b}")
+print(f"Произведение двух чисел = {a * b}")
