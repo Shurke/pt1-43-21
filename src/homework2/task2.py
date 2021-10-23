1577654704f66b5c17133d77967beac5c5a86bcd
Miron Chuduk <miron@chuduk.by>
2021-10-23 22:27:49 +0300

    Task 2
    Самое длинное слово в введенном предложении, учитывая, что в предложении есть знаки препинания.

---
+++ src/homework2/task2.py
@@ -0,0 +1,11 @@
+import string
+my_string = input()
+for x in my_string:
+    if x in string.punctuation:
+        my_string = my_string.replace(x, "")
+result = ""
+new_string = my_string.split()
+for i in new_string:
+    if len(i) > len(result):
+        result = i
+print(f"Самое длинное слово в предложении: {result}")
