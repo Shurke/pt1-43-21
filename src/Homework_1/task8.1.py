2faf2f8af5451ced62385770b35bd469275d6c79
MrGo1d <alejandroustin@gmail.com>
2021-10-27 21:00:18 +0300

    removed spaces from task

--- src/Homework_1/task8.1.py
+++ src/Homework_1/task8.1.py
@@ -4,18 +4,16 @@ https://py.checkio.org/ , https://www.codewars.com, https://www.hackerrank.com/,
 https://acmp.ru И решите 1-5 задач уровня Elementary и advanced. Поместите 3 простых
 и 2 сложных задачи на Ваш выбор в пул реквест.
 Эти задачи поместите в файл task8.py в папке src/homework2."""
-
 # задачи решал на codewar
 
-"""Pete likes to bake some cakes. He has some recipes and ingredients. 
-Unfortunately he is not good in maths. Can you help him to find out, how many 
+"""Pete likes to bake some cakes. He has some recipes and ingredients.
+Unfortunately he is not good in maths. Can you help him to find out, how many
 cakes he could bake considering his recipes?
 
-Write a function cakes(), which takes the recipe (object) and the available 
-ingredients (also an object) and returns the maximum number of cakes Pete can bake (integer). 
-For simplicity there are no units for the amounts 
-(e.g. 1 lb of flour or 200 g of sugar are simply 1 or 200). Ingredients that are not present 
-in the objects, can be considered as 0.
+Write a function cakes(), which takes the recipe (object) and the available ingredients
+(also an object) and returns the maximum number of cakes Pete can bake (integer).
+For simplicity there are no units for the amounts (e.g. 1 lb of flour or 200 g of sugar are 
+simply 1 or 200). Ingredients that are not present in the objects, can be considered as 0.
 
 Examples:
 
