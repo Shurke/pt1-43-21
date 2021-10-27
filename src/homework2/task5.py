ea7efa584242ad6557bbe69a9c4c9bd89f8f80ca
crankymonk <dj_andrej@mail.ru>
2021-10-28 00:35:34 +0300

    Fix code for flake8

--- src/homework2/task5.py
+++ src/homework2/task5.py
@@ -1,15 +1,16 @@
-#5. Выведите n-ое число Фибоначчи, используя только временные переменные, циклические операторы и условные операторы.
-#n - вводится
+# 5. Выведите n-ое число Фибоначчи, используя только временные переменные,
+# циклические операторы и условные операторы.
+# n - вводится
 m = int(input('Enter a number: '))
 n = m - 1
 a = 1
-l = [0, 1]
-x = l[1]
+fib = [0, 1]
+x = fib[1]
 
 for a in range(m - a):
     if a < m - 1:
-        x = x + l[a]
-        l.append(x)
+        x = x + fib[a]
+        fib.append(x)
         a += 1
 
-print(str(m) + 'th Fibonacci number is - ' + str(l[n]))
+print(str(m) + 'th Fibonacci number is - ' + str(fib[n]))
--- src/homework2/task8.5.py
+++ src/homework2/task8.5.py
@@ -1,5 +1,7 @@
-#Given an array of ones and zeroes, convert the equivalent binary value to an integer.
-#Eg: [0, 0, 0, 1] is treated as 0001 which is the binary representation of 1.
+# Given an array of ones and zeroes,
+# convert the equivalent binary value to an integer.
+# Eg: [0, 0, 0, 1] is treated as 0001 which is
+# the binary representation of 1.
 
 
 arr = [0, 1, 0, 1]
