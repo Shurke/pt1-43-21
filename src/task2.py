7297457b0f0117cf01c67af51147ebd602b0d5e9
KirPython <93128318+KirPython@users.noreply.github.com>
2021-10-28 21:25:52 +0300

    Update task2.py

--- src/task2.py
+++ src/task2.py
@@ -2,12 +2,16 @@
 Найти самое длинное слово в введенном предложении. Учтите что в предложении
 есть знаки препинания.
 """
-str1 = input("Введите любое предложение: ")
-str1 = str1.split()
-max_word = ""
-max_word_len = 0
-for v in str1:
-    if len(v) > max_word_len:
-        # max_word_len = len(v) 
-        max_word = v
-print('Самое длинное слово в предложении: ', max_word)
+def main()
+    str1 = input("Введите любое предложение: ")
+    str1 = str1.split()
+    max_word = ""
+    max_word_len = 0
+        for v in str1:
+            if len(v) > max_word_len:
+            # max_word_len = len(v) 
+            max_word = v
+    print('Самое длинное слово в предложении: ', max_word)
+    
+    if __name__ == "__main__":
+    main()
