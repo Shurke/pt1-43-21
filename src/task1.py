8aaa5640efe418ef77db6a9de706b465717c0b18
KirPython <93128318+KirPython@users.noreply.github.com>
2021-10-28 18:33:43 +0300

    Update task1.py

--- src/task1.py
+++ src/task1.py
@@ -7,5 +7,5 @@ def main():
 price = float(input("Введите цену товара: "))
 qty = int(input("Введите количество товара:  "))
 print("Сумма равняется: ", price * qty)
-if__name__ == "__main__":
+if __name__ == "__main__":
   main()
