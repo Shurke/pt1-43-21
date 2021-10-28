00f01f30198fdaab74825ed36bd75203386ba303
KirPython <93128318+KirPython@users.noreply.github.com>
2021-10-28 21:29:01 +0300

    Update task5.py

--- src/task5.py
+++ src/task5.py
@@ -2,18 +2,22 @@
 Выведите n-ое число Фибоначчи, используя только временные переменные,
 циклические операторы и условные операторы. n - вводится
 """
-n = int(input("Введите N: "))
-if n == 0:
-    print('0')
-elif n == 1:
-    print('1')
-else:
-    prev1 = 0
-    prev2 = 1
-    index = 3
-    while index <= n:
-        cur = prev1 + prev2
-        index = index + 1
-        prev1 = prev2
-        prev2 = cur
-print(cur)
+def main()
+    n = int(input("Введите N: "))
+    if n == 0:
+        print('0')
+    elif n == 1:
+        print('1')
+    else:
+        prev1 = 0
+        prev2 = 1
+        index = 3
+        while index <= n:
+            cur = prev1 + prev2
+            index = index + 1
+            prev1 = prev2
+            prev2 = cur
+    print(cur)
+    
+    if __name__ == "__main__":
+    main()
