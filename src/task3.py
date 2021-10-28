f86f5454825e4810c64e2e7309533d0612ed994d
KirPython <93128318+KirPython@users.noreply.github.com>
2021-10-28 21:27:02 +0300

    Update task3.py

--- src/task3.py
+++ src/task3.py
@@ -2,11 +2,15 @@
 Вводится строка. Требуется удалить из нее повторяющиеся символы и все пробелы.
 Например, если было введено "abc cde def", то должно быть выведено "abcdef".
 """
-s = input('Введите предложение: ')
-s = s.replace(' ', '')
-s = list(s)
-new_s = ''
-for n in s:
-    if new_s.find(n) == -1:
-        new_s = new_s + n
-print(new_s)
+def main()
+    s = input('Введите предложение: ')
+    s = s.replace(' ', '')
+    s = list(s)
+    new_s = ''
+    for n in s:
+        if new_s.find(n) == -1:
+            new_s = new_s + n
+    print(new_s)
+    
+    if __name__ == "__main__":
+    main()
