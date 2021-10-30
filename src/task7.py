614055e5d4936f5416be1b421b29a0cedddc098d
KirPython <93128318+KirPython@users.noreply.github.com>
2021-10-30 15:01:21 +0300

    Update task7.py

--- src/task7.py
+++ src/task7.py
@@ -11,10 +11,11 @@ def main():
     side1 = float(input("Введите первую сторону треугольника (см): "))
     side2 = float(input("Введите вторую сторону треугольника (см): "))
     side3 = float(input("Введите третью сторону треугольника (см): "))
-    if side1 < (side2 + side3) and side2  < (side1 + side3) and side3 < (side1 + side2 ):
+    if side1 < (side2 + side3) and side2 < (side1 + side3) and side3 < (side1 + side2):
         semi_perimeter = (side1 + side2 + side3) / 2   # полупериметр
         # площадь треугольника по формуле Герона
-        square = (semi_perimeter * (semi_perimeter - side1) * (semi_perimeter - side2) * (semi_perimeter - side3)) ** 0.5
+        square = ((semi_perimeter * (semi_perimeter - side1) * (semi_perimeter - side2) 
+                   * (semi_perimeter - side3)) ** 0.5)
         print("Площадь треугольника равняется (см2)", square)
     else:
         print("Неверные данные")
