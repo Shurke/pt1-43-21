86ae1679e0b7c4d6461e12f2aabe4848184de33a
KirPython <93128318+KirPython@users.noreply.github.com>
2021-10-30 13:57:47 +0300

    Update task2.py

--- src/task2.py
+++ src/task2.py
@@ -9,10 +9,10 @@ def main():
     str1 = str1.split()
     max_word = ""
     max_word_len = 0
-        for v in str1:
-            if len(v) > max_word_len:
-            # max_word_len = len(v) 
-            max_word = v
+    for v in str1:
+        if len(v) > max_word_len:
+        # max_word_len = len(v) 
+        max_word = v
     print('Самое длинное слово в предложении: ', max_word)
 
 
