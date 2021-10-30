241ed648cefb55233df4625d00d4197fcbddc39f
Andrei Kadushko <kadushko@gmail.com>
2021-10-30 20:03:15 +0300

    Change task8-3

--- src/Homework2/task8_3.py
+++ src/Homework2/task8_3.py
@@ -1,13 +1,11 @@
 def printer_error(s):
-
-    """Sometimes there are problems: lack of colors, technical malfunction and
-    a "bad" control string is produced e.g. aaaxbbbbyyhwawiwjjjwwm
-    with letters not from a to m.
-    You have to write a function printer_error which given a string
-    will return the error rate of the printer as a string representing
-    a rational whose numerator is the number of errors and the denominator
-    the length of the control string. Don't reduce this fraction to a simpler expression.
-    """
+    # Sometimes there are problems: lack of colors, technical malfunction and
+    # a "bad" control string is produced e.g. aaaxbbbbyyhwawiwjjjwwm
+    # with letters not from a to m.
+    # You have to write a function printer_error which given a string
+    # will return the error rate of the printer as a string representing
+    # a rational whose numerator is the number of errors and the denominator
+    # the length of the control string. Don't reduce this fraction to a simpler expression.
 
     k = 0
     i = ''
