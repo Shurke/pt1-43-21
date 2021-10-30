de116743604b01e8cab496b336701f1bc2db7186
KirPython <93128318+KirPython@users.noreply.github.com>
2021-10-30 14:48:23 +0300

    Update task4.py

--- src/task4.py
+++ src/task4.py
@@ -1,20 +1,23 @@
+"""Task4"""
+
+
 def main():
     """Посчитать количество строчных (маленьких) и прописных (больших) букв в введенной строке.
 
     Учитывать только английские буквы.
     """
-    y = input("Введите любую строку: ")
-    y = y.replace(' ', '')
-    y = list(y)
+    st = input("Введите любую строку: ")
+    st = st.replace(' ', '')
+    st = list(st)
     sum_l = 0
     sum_u = 0
     english_letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    for n in y:
-        if english_letters.find(n) != -1:
-            if n == n.lower():
-                sum_l = sum_l + 1
-            if n == n.upper():
-                sum_u = sum_u + 1
+    for letter in st:
+        if english_letters.find(letter) != -1:
+            if letter == letter.lower():
+                sum_l += 1
+            if letter == letter.upper():
+                sum_u += 1
     print("Общее количество строчных(маленьких)букв равняется", sum_l)
     print("Общее количество прописных(больших)букв равняется", sum_u)
 
