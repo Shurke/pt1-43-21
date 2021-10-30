4e0609a7cf1cc3cb3378f1faae437753b02fb729
Andrei Kadushko <kadushko@gmail.com>
2021-10-30 19:47:48 +0300

    Change 8_2

--- src/Homework2/task8_2.py
+++ src/Homework2/task8_2.py
@@ -1,6 +1,6 @@
 def persistence(n):
-
-    """Write a function, persistence, that takes in a positive parameter num
+    """
+    Write a function, persistence, that takes in a positive parameter num
     and returns its multiplicative persistence, which
     is the number of times you must multiply the digits
     in num until you reach a single digit
