927c5fc7963181df0f56cc014a4e89591092c1f4
tkal <tkal@gurtam.com>
2021-10-31 17:39:07 +0300

    homework2 tasks

---
+++ src/homework2/task4.py
@@ -0,0 +1,29 @@
+'''
+4. Посчитать количество строчных (маленьких)
+и прописных (больших) букв в введенной строке.
+Учитывать только английские буквы.
+'''
+
+
+def main():
+
+    import string
+
+    print('Введите произвольную строку')
+    msg = input()
+    eng_lower = 0
+    eng_upper = 0
+
+    for i in msg:
+        if i in string.ascii_lowercase:
+            eng_lower = eng_lower + 1
+        elif i in string.ascii_uppercase:
+            eng_upper = eng_upper + 1
+        else:
+            pass
+
+    print('Всего строчных английских букв:', eng_lower)
+    print('Всего прописных английских букв:', eng_upper)
+
+
+main()
---
+++ src/homework2/task8_3.py
@@ -0,0 +1,33 @@
+'''
+Task 3
+A square of squares
+Task
+Given an integral number, determine if it's a square number:
+In mathematics, a square number or perfect square is an integer that is the square of an integer;
+in other words, it is the product of some integer with itself.
+The tests will always use some integral number,
+so don't worry about that in dynamic typed languages.
+test.assert_equals(is_square(-1), False, "-1: Negative numbers cannot be square numbers")
+test.assert_equals(is_square( 0), True, "0 is a square number (0 * 0)")
+test.assert_equals(is_square( 3), False, "3 is not a square number")
+test.assert_equals(is_square( 4), True, "4 is a square number (2 * 2)")
+test.assert_equals(is_square(25), True, "25 is a square number (5 * 5)")
+test.assert_equals(is_square(26), False, "26 is not a square number")
+'''
+
+
+def is_square(n):
+
+    if n >= 0:
+        if n ** 0.5 % 1 == 0:
+            return True
+        else:
+            return False
+    else:
+        return False
+
+
+print('Task3 result')
+print(is_square(3))
+print(is_square(26))
+print(is_square(25))
