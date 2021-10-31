3657b483e64cc886364ee81ab93de23c4d90109f
crankymonk <dj_andrej@mail.ru>
2021-10-31 17:13:32 +0300

    Rework for PR plus flake8 correction

--- src/homework2/task7.py
+++ src/homework2/task7.py
@@ -12,9 +12,9 @@ side_c = int(input('Введите сторону c: '))
 if (side_a < side_b + side_c) and (side_b < side_a + side_c) and (side_c < side_a + side_b):
     half_perimeter = (side_a + side_b + side_c) / 2
     triangle_square = ((half_perimeter *
-                        ((half_perimeter - side_a)
-                            * (half_perimeter - side_b)
-                            * (half_perimeter - side_c))
+                        ((half_perimeter - side_a) *
+                         (half_perimeter - side_b) *
+                            (half_perimeter - side_c))
                         ) ** 0.5)  # Формула Герона для площади треугольника
     print('Площадь треугольника: ' + str(triangle_square))
 else:
