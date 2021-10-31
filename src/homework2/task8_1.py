6ad05aa7dc7d9cc5feeb4b1dfa3e8261c6dfd79c
tkal <tkal@gurtam.com>
2021-10-31 16:11:32 +0300

    homework2 all tasks

---
+++ src/homework2/task8_1.py
@@ -0,0 +1,32 @@
+'''
+Task 1
+Write a function that accepts an array of 10 integers (between 0 and 9),
+that returns a string of those numbers in the form of a phone number.
+Example
+create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) # => returns "(123) 456-7890"
+test.assert_equals(create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]), "(123) 456-7890")
+test.assert_equals(create_phone_number([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]), "(111) 111-1111")
+test.assert_equals(create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]), "(123) 456-7890")
+test.assert_equals(create_phone_number([0, 2, 3, 0, 5, 6, 0, 8, 9, 0]), "(023) 056-0890")
+test.assert_equals(create_phone_number([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]), "(000) 000-0000")
+'''
+
+def create_phone_number(n):
+    res_str = '('
+    for i in range(10):
+        if i <= 2 or (i > 3 and i <= 5) or i > 6:
+            res_str = res_str + str(n[i])
+        elif i == 3:
+            res_str = res_str + ') ' + str(n[i])
+        elif i == 6:
+            res_str = res_str + '-' + str(n[i])
+    return res_str
+
+
+print('Task1 result')
+print(create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]))
+print(create_phone_number([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]))
+print(create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]))
+print(create_phone_number([0, 2, 3, 0, 5, 6, 0, 8, 9, 0]))
+print(create_phone_number([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]))
+
