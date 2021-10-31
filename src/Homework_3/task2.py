9eaee2e1c639cc10db07b71101e3f653eef67c0e
MrGo1d <alejandroustin@gmail.com>
2021-10-31 10:27:50 +0300

    All tasks

--- src/Homework_3/task2.py
+++ src/Homework_3/task2.py
@@ -1,11 +1,23 @@
-"""    2. List practice
+"""
+2. List practice
 1. Используйте генератор списков чтобы получить следующий: ['ab', 'ac', 'ad', 'bb', 'bc', 'bd'].
 2. Используйте на предыдущий список slice чтобы получить следующий: ['ab', 'ad', 'bc'].
 3. Используйте генератор списков чтобы получить следующий ['1a', '2a', '3a', '4a'].
 4. Одной строкой удалите элемент  '2a' из прошлого списка и напечатайте его.
 5. Скопируйте список и добавьте в него элемент '2a' так чтобы в исходном списке этого элемента
-не было."""
+не было.
+"""
 
-lst = [i + j for i in ['a', 'b'] for j in ['b', 'c', 'd']]
+from copy import copy
 
-print(lst)
+lst = [i + j for i in 'ab' for j in 'bcd']
+# step#1
+lst_slice = lst[::2]
+# step#2
+lst_num = [str(i) + 'a' for i in range(1, 5)]
+# step#3
+print(lst_num.pop(1))
+# step#4
+lst_copy = (copy(lst_num))
+lst_copy.append('2a')
+# step#5
