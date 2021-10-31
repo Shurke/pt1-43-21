18f8d2d8b674fba12daf59afcb31f7aef98a1da3
crankymonk <dj_andrej@mail.ru>
2021-10-31 17:17:46 +0300

    Rework for PR with flake8 correction2

--- src/homework2/task7.py
+++ src/homework2/task7.py
@@ -10,12 +10,12 @@ side_b = int(input('Введите сторону b: '))
 side_c = int(input('Введите сторону c: '))
 
 if (side_a < side_b + side_c) and (side_b < side_a + side_c) and (side_c < side_a + side_b):
-    half_perimeter = (side_a + side_b + side_c) / 2
-    triangle_square = ((half_perimeter *
-                        ((half_perimeter - side_a) *
-                         (half_perimeter - side_b) *
-                            (half_perimeter - side_c))
-                        ) ** 0.5)  # Формула Герона для площади треугольника
+    half_p = (side_a + side_b + side_c) / 2
+
+    # Формула Герона для площади треугольника
+    triangle_square = (
+        (half_p * ((half_p - side_a) * (half_p - side_b) * (half_p - side_c))) ** 0.5
+    )
     print('Площадь треугольника: ' + str(triangle_square))
 else:
     print('Треугольника с заданными сторонами не существует.')
