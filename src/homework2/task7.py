9fe0eb206bb2ac264bd629de3213d2bcab199106
Miron Chuduk <miron@chuduk.by>
2021-11-01 15:25:58 +0300

    Reducing code

--- src/homework2/task7.py
+++ src/homework2/task7.py
@@ -6,28 +6,11 @@ side2 = float(input("Сторона 2: "))
 side3 = float(input("Сторона 3: "))
 if side1 > 0 and side2 > 0 and side3 > 0:
     p = (side1 + side2 + side3) / 2
-    if side1 >= side2 and side1 >= side3:
-        if side1 < side2 + side3:
-            print("Существует!")
-            S = (p * (p - side1) * (p - side2) * (p - side3)) ** 0.5
-            print(f"Площадь: {S}")
-        else:
-            print("Существует!")
-            S = (p * (p - side1) * (p - side2) * (p - side3)) ** 0.5
-            print(f"Площадь: {S}")
-    elif side2 >= side1 and side2 >= side3:
-        if side2 < side1 + side3:
-            print("Существует!")
-            S = (p * (p - side1) * (p - side2) * (p - side3)) ** 0.5
-            print(f"Площадь: {S}")
-        else:
-            print("Не существует!")
+    if side1 + side2 > side3 and side1 + side3 > side2 and side2 + side3 > side1:
+        print("Существует!")
+        S = (p * (p - side1) * (p - side2) * (p - side3)) ** 0.5
+        print(f"Площадь: {S}")
     else:
-        if side3 < side1 + side2:
-            print("Существует!")
-            S = (p * (p - side1) * (p - side2) * (p - side3)) ** 0.5
-            print(f"Площадь: {S}")
-        else:
-            print("Не существует!")
+        print("Не существует!")
 else:
     print("Ошибка ввода!")
