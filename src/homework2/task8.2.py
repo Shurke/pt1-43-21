bb401ddd4ab5c87d5fb268a97c838fa3eff53b34
Miron Chuduk <miron@chuduk.by>
2021-11-03 23:50:58 +0300

    Deleting last homework

--- src/homework2/task8.2.py
+++
@@ -1,6 +0,0 @@
-# You are given the firstname and lastname of a person on two different lines.
-# Your task is to read them and print the following
-# Hello firstname lastname! You just delved into python.
-firstname = input("Firstname: ")
-lastname = input("Lastname: ")
-print(f"Hello {firstname} {lastname}! You just delved into python.")
--- src/homework2/task8.4.py
+++
@@ -1,9 +0,0 @@
-# You are given a string. Split the string on a " "
-# delimiter and join using a - hyphen.
-print("Введите строку состоящую из нескольких слов через пробел")
-string = input("Строка: ")
-new_string = ""
-for i in string.split():
-    new_string = new_string + '-' + i
-new_string = new_string.strip("-")
-print(new_string)
