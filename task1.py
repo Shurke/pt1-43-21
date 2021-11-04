0c673212433bfb3cafb9670dea7f8b80b5f1a824
Alexei1090 <91412534+Alexei1090@users.noreply.github.com>
2021-11-04 11:23:35 +0300

    Add files via upload

--- task1.py
+++ task1.py
@@ -1,5 +1,15 @@
-m = int(input('цена рублей'))
-n = int(input('коппеек'))
-s = int(input('колл. товара'))
+"""Напишите программу, которая печатает цифры от 1 до 100, 
+но вместо чисел, кратных 3 пишет Fizz, вместо чисел кратный 5 пишет Buzz, 
+а вместо чисел одновременно кратных и 3 и 5 - FizzBuzz
+"""
 
-print((m * s + n * s // 100), (n * s % 100))
+
+for n in range(1, 101):
+    if n % 3 == 0 and n % 5 == 0:
+        print("FizzBuzz", end=' ')
+    elif n % 3 == 0:
+        print("Fizz", end=' ')
+    elif n % 5 == 0:
+        print("Buzz", end=' ')
+    else:
+        print(n, end=' ')
