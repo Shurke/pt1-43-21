2609744169aa2f1e575c6e592c79c77ab9d400a6
crankymonk <dj_andrej@mail.ru>
2021-11-06 21:46:39 +0300

    HW5 - Add task7

---
+++ src/homework5/task7.py
@@ -0,0 +1,15 @@
+'''
+Оглянемся назад. Числа
+Даны два натуральных числа. Вычислите их наибольший общий делитель
+при помощи алгоритма Евклида (мы не знаем функции и рекурсию).
+'''
+
+
+first_num = int(input('Enter first number: '))
+second_num = int(input('Enter second number: '))
+
+while first_num % second_num > 0:
+    great_comm_divisor = first_num % second_num
+    first_num, second_num = second_num, great_comm_divisor
+
+print(great_comm_divisor)
