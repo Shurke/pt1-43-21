3d72b7b6c72422a627a74133762785b0ba2806d6
crankymonk <dj_andrej@mail.ru>
2021-11-06 18:58:05 +0300

    HW5 - Add task1

--- src/homework2/task3.py
+++
@@ -1,9 +0,0 @@
-st1 = input('Enter "abrakadabra"')
-st = st1.replace(' ', '')
-
-a = st[0]
-for i in st:
-    if i not in a:
-        a = a + i
-
-print(a)
---
+++ src/homework5/task1.py
@@ -0,0 +1,2 @@
+dict_w_cubes = {key: key ** 3 for key in range(1, 21)}
+print(dict_w_cubes)
