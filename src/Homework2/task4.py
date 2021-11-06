53cdd8affa8db60d5526cd84100290311d18abc4
Andrei Kadushko <kadushko@gmail.com>
2021-11-06 20:49:55 +0300

    change task4...7

--- src/Homework2/task4.py
+++ src/Homework2/task4.py
@@ -2,13 +2,14 @@
 Посчитать количество строчных (маленьких) и прописных (больших) букв в
 введенной строке. Учитывать только английские буквы.
 """
+
 my_string = input('Введите строку: ')
-k = 0
-m = 0
+lower_letter = 0
+upper_letter = 0
 for i in my_string:
     if i.islower():
-        k += 1
+        lower_letter += 1
     elif i.isupper():
-        m += 1
-print('Строчных букв:', k)
-print('Прописных букв:', m)
+        upper_letter += 1
+print('Строчных букв:', lower_letter)
+print('Прописных букв:', upper_letter)
