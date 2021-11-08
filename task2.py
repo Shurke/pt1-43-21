91f5a7285a8e9a82781d6f33c714d71c3c219697
Alexei1090 <91412534+Alexei1090@users.noreply.github.com>
2021-11-09 00:24:38 +0300

    Add files via upload

--- task2.py
+++ task2.py
@@ -17,8 +17,8 @@ for _ in range(int(input())):
     state, *cities = input().split()
     for city in cities:
         states[city] = state
- 
-print(*(states[input()] for _ in range(int(input()))),sep = "\n")
+
+print(*(states[input()] for _ in range(int(input()))), sep="\n")
 
 
 """Тут совсем тяжело. Пока не понимаю про распаковку"""
