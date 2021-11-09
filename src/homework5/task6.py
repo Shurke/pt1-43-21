0f225572c099a0f89d115526bfde4138a96b3ed4
Miron Chuduk <miron@chuduk.by>
2021-11-09 15:08:46 +0300

    Task 6

---
+++ src/homework5/task6.py
@@ -0,0 +1,16 @@
+"""
+Во входной строке записан текст. Словом считается последовательность
+непробельных символов идущих подряд, слова разделены одним или
+большим числом пробелов или символами конца строки.
+Определите, сколько различных слов содержится в этом тексте.
+"""
+
+
+import string
+
+my_string = input("Введите строку:\n")
+for symbol in my_string:
+    if symbol in string.punctuation:
+        my_string = my_string.replace(symbol, "")
+my_list = my_string.split()
+print(f"Всего слов: {len(my_list)}")
