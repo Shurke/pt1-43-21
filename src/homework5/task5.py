2d32204abc062bebb6ddeffdaa1d998bb7c209c4
tkal <tkal@gurtam.com>
2021-11-09 15:29:59 +0300

    task5 added

---
+++ src/homework5/task5.py
@@ -0,0 +1,63 @@
+'''
+5.Языки
+Каждый из N школьников некоторой школы знает Mi языков.
+Определите, какие языки знают все школьники и языки,
+которые знает хотя бы один из школьников.
+
+Входные данные
+Первая строка входных данных содержит количество школьников N.
+Далее идет N чисел Mi, после каждого из чисел идет Mi строк,
+содержащих названия языков, которые знает i-й школьник.
+
+Пример входных данных:
+3          # N количество школьников
+2          # M1 количество языков первого школьника
+Russian    # языки первого школьника
+English
+3          # M2 количество языков второго школьника
+Russian
+Belarusian
+English
+3
+Russian
+Italian
+French
+
+Выходные данные
+В первой строке выведите количество языков, которые знают все школьники.
+Начиная со второй строки - список таких языков.
+Затем - количество языков, которые знает хотя бы один школьник,
+на следующих строках - список таких языков.
+'''
+
+
+def main():
+    all_students_dct = {}
+    one_lang = set()
+    # input processing
+    for ind1 in range(int(input('Введите количество школьников:'))):
+        languages_lst = []
+        for ind2 in range(int(input('Введите количество языков:'))):
+            languages_lst.append(input('Введите язык:'))
+        one_lang = one_lang | set(languages_lst)
+        all_students_dct[ind1] = languages_lst
+
+    # output processing
+    # If leave common_lang as (), the result will be empty set
+    common_lang = set(all_students_dct.get(0))
+    for ind1 in range(len(all_students_dct.values())):
+        tmp_set = set(all_students_dct.get(ind1))
+        common_lang = common_lang & tmp_set
+
+    print('Кол-во общих языков:', len(common_lang), 'Языки:')
+    print('Языки:')
+    for ind1 in common_lang:
+        print(ind1)
+
+    print('Кол-во общих языков, которые знает хотя бы один школьник:', len(one_lang))
+    print('Языки:')
+    for ind1 in one_lang:
+        print(ind1)
+
+
+main()
