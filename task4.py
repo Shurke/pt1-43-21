3dc45c8ffc1d32d8461b5f477a0891288e9849d2
Alexei1090 <91412534+Alexei1090@users.noreply.github.com>
2021-11-13 18:07:05 +0300

    Add files via upload

--- task4.py
+++ task4.py
@@ -1,12 +1,18 @@
+"""Посчитать количество строчных (маленьких) и прописных (больших) букв в введенной строке. 
+Учитывать только английские буквы."""
+
+
+import string
+
 a = str(input('Введите предложение', ))
 am = 0
 ab = 0
 
 for i in a:
-    if i.islower():
-        am += 1
-    else:
-        ab += 1
+    if i in string.ascii_lowercase:
+        am = am + 1
+    elif i in string.ascii_uppercase:
+        ab = ab + 1
 
 print('больших', ab)
 print('маленьких', am)
