3d4dcf22a88ce2c08cd39eb5cc8d4c1eb58bebca
tkal <tkal@gurtam.com>
2021-11-15 23:02:32 +0300

    task2 solution fix

--- src/homework2/task2.py
+++ src/homework2/task2.py
@@ -15,19 +15,17 @@ def main():
     msg = input()
     words = ''
     max_word = ''
-    max_len = 0
+    words_lst = []
 
     for i in msg:
         if i not in string.punctuation:
             words = words + i
-    words = words.split()
+        else:
+            words = words + ' '
+    words_lst = words.split()
+    max_word = max(words_lst, key=len)
 
-    for i in words:
-        if len(i) > max_len:
-            max_len = len(i)
-            max_word = i
-
-    print("Самое длинное слово:", max_word, ", его длина составляет", max_len, "символов")
+    print("Самое длинное слово:", max_word, ", его длина составляет", len(max_word), "символов")
 
 
 main()
