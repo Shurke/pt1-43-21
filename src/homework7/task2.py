cc9fa6c7e1cf963982ef71ef9088fa399c7c04f9
tkal <tkal@gurtam.com>
2021-11-19 22:25:11 +0300

    task2 added

---
+++ src/homework7/task2.py
@@ -0,0 +1,24 @@
+'''
+Создайте декоратор, который хранит результаты вызовов функции
+(за все время вызовов, не только текущий запуск программы)
+'''
+
+
+def dec(fun):
+    num = 0
+    def wrapper(*args, **kwargs):
+        nonlocal num
+        print('Call num:')
+        num += fun()
+        return num
+    return wrapper
+
+
+@dec
+def func():
+  return 1
+
+
+print(func())
+print(func())
+print(func())
