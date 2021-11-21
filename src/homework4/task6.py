859282dc19058e45d27d49f452200b59aa3be6d3
KirPython <93128318+KirPython@users.noreply.github.com>
2021-11-21 05:48:09 +0300

    Update task6.py

--- src/homework4/task6.py
+++ src/homework4/task6.py
@@ -13,7 +13,7 @@ def main():
         if element == 0:
             lst.append(element)
             lst.remove(element)
-    print(lst)
+    print('Результат:', lst)
 
 
 if __name__ == "__main__":
