c6f3dbb97f6aee9da91935849bf8d1969c2ecbd3
ranok3 <ales.rovba@gmail.com>
2021-11-21 22:07:38 -0500

    task1 done

--- src/homework7/task1.py
+++ src/homework7/task1.py
@@ -32,33 +32,24 @@ import t8_3_home
 import t8_4_home
 import t8_home
 
-
-def runner():
-    t1_home.func()
-    t2_home.func()
-    t3_home.func()
-    t4_home.func()
-    t5_home.func()
-    t6_home.func()
-    t7_home.func()
-    t8_home.func()
-    t8_1_home.func()
-    t8_2_home.func()
-    t8_3_home.func()
-    t8_4_home.func()
-    t3_1_home.func()
-    t3_2_home.func()
-    t3_3_home.func()
-    t3_4_home.func()
-    t3_5_home.func()
-    t3_6_home.func()
-    t5_1_home.func()
-    t5_2_home.func()
-    t5_3_home.func()
-    t5_4_home.func()
-    t5_5_home.func()
-    t5_6_home.func()
-    t5_7_home.func()
+f = [t1_home.func, getattr(t2_home, 'func'), getattr(t3_home, 'func'), getattr(t4_home, 'func'),
+     getattr(t5_home, 'func'), getattr(t6_home, 'func'), getattr(t7_home, 'func'), getattr(t8_home, 'func'),
+     getattr(t8_1_home, 'func'), getattr(t8_2_home, 'func'), getattr(t8_3_home, 'func'), getattr(t8_4_home, 'func'),
+     getattr(t3_1_home, 'func'), getattr(t3_2_home, 'func'), getattr(t3_3_home, 'func'), getattr(t3_4_home, 'func'),
+     getattr(t3_5_home, 'func'), getattr(t3_6_home, 'func'), getattr(t5_1_home, 'func'), getattr(t5_2_home, 'func'),
+     getattr(t5_3_home, 'func'), getattr(t5_4_home, 'func'), getattr(t5_5_home, 'func'), getattr(t5_6_home, 'func'),
+     getattr(t5_7_home, 'func')]
 
 
-runner()
+def runner(*args):
+    if len(args) == 0:
+        for function in f:
+            print(function.__name__)
+    else:
+        for name in args:
+            for function_ in f:
+                if function_.__name__ == name:
+                    function_()
+
+
+runner('func')
