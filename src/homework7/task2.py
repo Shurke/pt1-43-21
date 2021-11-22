995d5e22a2e6fbfe5ef8933bef64ef6e9e38fa3f
tkal <tkal@gurtam.com>
2021-11-22 22:19:54 +0300

    task2 reworked

--- src/homework7/task2.py
+++ src/homework7/task2.py
@@ -3,23 +3,25 @@
 (за все время вызовов, не только текущий запуск программы)
 '''
 
+import datetime
+
 
 def dec(fun):
-    num = 0
-
     def wrapper(*args, **kwargs):
-        nonlocal num
-        print('Call num:')
-        num += fun()
-        return num
+        dat = str(datetime.datetime.now())
+        tmp_file = open("task2_mem.txt", "a")
+        result = fun(*args, **kwargs)
+        tmp_file.writelines(dat + ' | ' + str(result) + '\n')
+        tmp_file.close()
+        return result
     return wrapper
 
 
 @dec
-def func():
-    return 1
+def func(a, b):
+  return a + b
 
 
-print(func())
-print(func())
-print(func())
+print(func(1, 2))
+print(func(3, 4))
+print(func(5, 6))
