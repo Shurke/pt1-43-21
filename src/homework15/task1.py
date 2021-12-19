3b5cd3e54cb57b222ca686e3c1258790f30a1d73
Elena Okun <tranzakcia25@rambler.ru>
2021-12-19 14:48:33 +0200

    task1

--- src/homework15/task1.py
+++ src/homework15/task1.py
@@ -1,4 +1,4 @@
-""" Задача 1. Реализована модель заказа пиццы в ресторане
+""" Задача 1. Реализована модель заказа пиццы из различных ингредиентов
 
 Создайте  модель из жизни. Это может быть бронирование комнаты в отеле,
 покупка билета в транспортной компании, или простая РПГ.
@@ -6,15 +6,20 @@
 Объекты должны содержать как атрибуты так и методы класса для симуляции
 различных действий. Программа должна инстанцировать объекты и эмулировать
 какую-либо ситуацию - вызывать методы, взаимодействие объектов и т.д.
-
 """
 import random
 
 
 class Order:
-    """осдержит список пицц, умеет посчитывать стоимость заказа"""
+    """Класс преднозначен для работы с заказом
+    Для создания экземпляра класса необходимо передать список пицц: list_of_pizzas
+    Умеет подсчитывать стоимость заказа: count_total"""
     def __init__(self, list_of_pizzas):
         self.list_of_pizzas = list_of_pizzas
+
+    def __str__(self):
+        list_of_pizza_names = map(str, self.list_of_pizzas)
+        return ", ".join(list_of_pizza_names)
 
     def count_total(self):
         total = 0
@@ -24,14 +29,24 @@ class Order:
 
 
 class Pizza:
-    """"""
+    """Класс предназначен для создания пиццы
+    Для создания экземпляра небходимо передать инредиенты: ingredients, и размер пиццы: size.
+    Хранит список возможных ингридитентов: available_ingredients, а так же их цены: ingredients_prices.
+    Хранит стоимость приготовления пиццы: cost_to_cook.
+    Хранит виды размеров пицы: sizes и коэффициенты изменения цены от размера: price_correction.
+    Умее расчитать цену пиццы: price"""
 
-    price_to_cook = 7
-    sizes = ("small", "medium", "big")
-    price_correction = dict()
-    price_correction["small"] = 1
-    price_correction["medium"] = 1.3
-    price_correction["big"] = 1.5
+    cost_to_cook = 7
+    sizes = (
+        "small",
+        "medium",
+        "big",
+    )
+    price_correction = {
+        "small": 1,
+        "medium": 1.3,
+        "big": 1.5,
+    }
     available_ingredients = (
         "морской коктейль из мидий",
         "мини-осьминоги",
@@ -81,45 +96,94 @@ class Pizza:
         self.ingredients = ingredients
         self.size = size
 
+    def __str__(self):
+        meat = {
+            "бекон",
+            "куриное филе",
+            "ветчина",
+        }
+        sea = {
+            "морской коктейль из мидий",
+            "мини-осьминоги",
+            "кальмары",
+            "креветки",
+        }
+        cheese = {
+            "Моцарелла",
+            "Пармезан",
+            "Эмменталь",
+            "сыр Дорблю",
+        }
+        vegetarian = {
+           "каперсы",
+           "маслины",
+           "петрушка",
+           "орегано",
+           "базилик",
+           "сельдерей",
+           "томатный соус",
+           "помидоры",
+        }
+        names_for_pizza = {
+            "Морская пицца": sea,       
+            "Мясная пицца": meat,
+            "Сырная пицца": cheese,
+            "Вегетарианская пицца": vegetarian,
+        }
+        set_ingredients = set(self.ingredients)
+        for name_of_pizza, main_ingredients in names_for_pizza.items():
+            if main_ingredients & set_ingredients:
+                return name_of_pizza
+        return "Оригинальная пицца"
+
     def price(self):
-        """счиатем в зависимости от размера и ингридиентов и стоимости услуги приготовления"""
-        total = self.price_to_cook
+        """Считаем в зависимости от размера и ингридиентов и стоимости услуги приготовления"""
+        total = self.cost_to_cook
         for ingredient in self.ingredients:
             total = total + self.ingredients_prices[ingredient]
         total = total * self.price_correction[self.size]
-        return total
+        return round(total, 2)
 
 
 class Waiter:
-
+    """Класс приеднозначени для работы с клиентом
+    При создании класса необходимо передать имя официанта: name
+    Умеет принимать заказ: take_oder
+    Умет отправлять заказ на кухню: sent_oder_to_cook
+    Умет принимать оплату take_payment"""
     def __init__(self, name):
         self.name = name
 
     def take_oder(self):
         list_of_pizzas = []
-        print("Добрый день, меня зовут " + self.name,
-              "Я готов принят ваш заказ, Давайте выберем пиццы.")
+        print("Добрый день, меня зовут " + self.name + ".",
+              "Я готов принять ваш заказ, Давайте выберем пиццы.")
         one_more_pizza = True
         while one_more_pizza:
             ingredients = []
             print("Выберите размер пиццы: ", ", ".join(Pizza.sizes))
-            size = "big"
+            size = self.random_choose(Pizza.sizes)
             print("Вы выбрали пиццу размера " + size)
             print("Вы можете выбрать следующие ингредиенты: ")
             for ingredient in Pizza.available_ingredients:
                 print("\033[1;34;47m", ingredient, "\033[0;0m")
-            ingredients.append("Пармезан")
-            ingredients.append("бекон")
+
+            ingredients_to_choose = list(Pizza.available_ingredients)
+            random_number_of_ingredients = random.randint(0, len(ingredients_to_choose) - 1)
+            for i in range(random_number_of_ingredients):
+                ingredient = self.random_choose(ingredients_to_choose)
+                ingredients.append(ingredient)
+                ingredients_to_choose.remove(ingredient)
 
             if len(ingredients) == 0:
                 print("К сожалению, вы не выбрали ингредиенты для пиццы")
                 one_more_pizza = False
             else:
-                print("Вы выбрали следующте ингредиенты: ", ", ".join(ingredients))
+                print("Вы выбрали следующте ингредиенты: ", ", ".join(ingredients), ". Хороший выбор.")
                 new_pizza = Pizza(ingredients, size)
                 list_of_pizzas.append(new_pizza)
             print("Готовы заказать еще одну пиццу?")
-            one_more_pizza = False
+            one_more_pizza = self.random_choose((True, False))
             print("ок")
 
         if len(list_of_pizzas) == 0:
@@ -130,14 +194,18 @@ class Waiter:
             return Order(list_of_pizzas)
 
     def sent_oder_to_cook(self, customer_order):
-        print("Ваш заказ будет готов чере 15 минут")
+        print("Кухня приняла заказ официанта " + self.name)
+        print("Ваш заказ ", customer_order, " будет готов чере 15 минут")
         return True
 
     def take_payment(self, customer_order):
         print("Сумма вашего заказа составляет: ", customer_order.count_total())
-        print("Карта или наличные?")
-        print("Оплата прошла успешно")
+        print("Офииант " + self.name + " принял оплату от клиента.")
         return True
+
+    @staticmethod
+    def random_choose(list_of_elements):
+        return list_of_elements[random.randint(0, len(list_of_elements) - 1)]
 
 
 waiter_Mark = Waiter("Mark")
