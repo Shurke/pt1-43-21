6b2d28f5ab113e10c11b7ea8d4a9f7dfad04f4b0
MrGo1d <alejandroustin@gmail.com>
2021-12-21 07:27:19 +0300

    mend

--- src/Homework_15/tests_task4.py
+++ src/Homework_15/tests_task4.py
@@ -9,28 +9,26 @@ import unittest
 class TestCaseTask4(unittest.TestCase):
     """Tests for task4 module"""
     def test_1(self):
-        """Test for one integer
-        :return: OK"""
+        """Test for one integer :return: OK"""
         self.assertEqual(task4(510510), 'n = 510510; max n/Euler(n) = 5.539388020833333')
 
     def test_2(self):
-        """Test for iter object for 1 to 8
-        :return: OK"""
+        """Test for iter object for 1 to 8 :return: OK"""
         self.assertEqual(task4([1, 2, 3, 4, 5, 6, 7, 8]), 'n = 6; max n/Euler(n) = 3.0')
 
     def test_3(self):
-        """Test for iter object from 1 to 8 in reverse order
-        :return: OK"""
+        """Test for iter object from 1 to 8 in reverse order :return: OK"""
         self.assertEqual(task4([8, 7, 6, 5, 4, 3, 2, 1]), 'n = 6; max n/Euler(n) = 3.0')
 
     def test_4(self):
-        """Test for two integer in tuple
-        :return: OK"""
+
+        """Test for two integer in tuple :return: OK"""
+
         self.assertEqual(task4((30030, 510510)), 'n = 510510; max n/Euler(n) = 5.539388020833333')
 
     def test_5(self):
-        """Test raise call's if type of iterable is not list or tuple
-        :return: OK"""
+        """Test raise call's if type of iterable is not list or tuple :return: OK"""
+        
         with self.assertRaises(TypeError) as error:
             task4({100000, 1000020, 1000011})
             self.assertEqual("The type of the iterable must be 'list' or 'tuple'",
