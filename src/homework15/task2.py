50070b5ed77450f88ca310b43c342de0e492b269
tkal <tkal@gurtam.com>
2021-12-26 16:40:14 +0300

    task2 added

---
+++ src/homework15/task2.py
@@ -0,0 +1,64 @@
+'''
+Создайте декоратор, который вызывает задекорированную функцию
+пока она не выполнится без исключений (но не более n раз - параметр декоратора).
+Если превышено количество попыток, должно быть возбуждено исключение типа TooManyErrors
+'''
+
+
+class TooManyErrors(Exception):
+    '''
+    TooManyErrors Class
+    '''
+
+    def __str__(self):
+        return 'TooManyErrors'
+
+
+def dec(fun):
+    """
+    Если превышено количество попыток {num_calls}, должно быть возбуждено исключение типа TooManyErrors
+    """
+    num_calls = int(input("Please enter call amount:"))
+
+    def wrapper(*args, **kwargs):
+        nonlocal num_calls
+        while num_calls > 0:
+            try:
+                result = fun(*args, **kwargs)
+                return result
+            except ValueError:
+                num_calls -= 1
+        if num_calls == 0:
+            raise TooManyErrors
+    return wrapper
+
+
+@dec
+def func_polindrome(num: int):
+    """
+    Определяет, является ли число палиндромом (читается слева направо и справа налево одинаково).
+    Число положительное целое, произвольной длины.
+    """
+
+    old_num = num
+    new_num = 0
+
+    while old_num > 0:
+        new_num = new_num * 10 + old_num % 10
+        old_num = old_num // 10
+    if num == new_num:
+        print('It is polindrome')
+    else:
+        print('It is not polindrome')
+        raise ValueError
+    return
+
+
+def main():
+    func_polindrome(123321)
+    func_polindrome(123)
+    func_polindrome(1111)
+
+
+if __name__ == '__main__':
+    main()
