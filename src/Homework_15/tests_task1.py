b21e00ee7e375bc05d0e02c1f95b803f63c13efd
MrGo1d <alejandroustin@gmail.com>
2021-12-26 15:27:27 +0300

    mend

--- src/Homework_15/tests_task1.py
+++ src/Homework_15/tests_task1.py
@@ -2,39 +2,35 @@
 Module for testing task1 Homework_15
 """
 
-import sys
 import unittest
+from task1 import Hero, Enemy
 from unittest import TestCase
 from ddt import ddt, data, unpack
-
-sys.path.append('Homework_15')
-import task1
 
 
 @ddt
 class TestClasses(TestCase):
     """Test case for task1"""
-
     @data((1, 1, 1, 1), (2, 1, 3, 1), (5, 5, 5, 1))
     @unpack
     def test_1(self, *args):
         """The test check, that 'inst' is instance of class 'Hero'"""
-        inst = task1.Hero(*args)
-        self.assertIsInstance(inst, task1.Hero)
+        inst = Hero(*args)
+        self.assertIsInstance(inst, Hero)
 
     @data((1, 1, 1, 1), (1, 2, 3, 1))
     @unpack
     def test_2(self, *args):
         """The test check, that 'inst' (instance of Enemy class) is not instance of class 'Hero'"""
-        inst = task1.Enemy(*args)
-        self.assertNotIsInstance(inst, task1.Hero)
+        inst = Enemy(*args)
+        self.assertNotIsInstance(inst, Hero)
 
     @data(('a', 1, 1, 1, 1), (1, 2, [], 'c', 1))
     @unpack
     def test_3(self, *args):
         """The test raises an exception when entering incorrect data"""
         with self.assertRaises(TypeError) as error:
-            task1.Hero(*args)
+            Hero(*args)
             self.assertEqual('Incorrect input: characteristic must be a integer!',
                              error.exception.args[0])
 
@@ -43,7 +39,7 @@ class TestClasses(TestCase):
     def test_4(self, *args):
         """The test raises an exception when entering negative number """
         with self.assertRaises(TypeError) as error:
-            task1.Hero(*args)
+            Hero(*args)
             self.assertEqual("Characteristics can't be less than 0!", error.exception.args[0])
 
 
