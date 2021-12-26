3094cacb6fc9f4740fdf854e023aa6ce44751218
crankymonk <dj_andrej@mail.ru>
2021-12-26 23:20:11 +0300

    Add task3 with tests

---
+++ src/homework15/task3.py
@@ -0,0 +1,34 @@
+'''
+Пары элементов
+Дан список чисел. Посчитайте, сколько в нем пар элементов, равных друг другу.
+Считается, что любые два элемента, равные друг другу образуют одну пару,
+которую необходимо посчитать.
+Входные данные - строка из чисел, разделенная пробелами.
+Выходные данные - количество пар.
+Важно: 1 1 1 - это 3 пары, 1 1 1 1 - это 6 пар
+'''
+
+import math
+
+
+def pair_counter_func(number_input: str = '1 1 1 1') -> int:
+    '''counts pairs in num_list'''
+    #number_input = input('Enter numbers with spaces between them: ')
+    number_input = number_input.replace(' ', '')
+    counted_numbers = []
+    pair_counter = 0
+
+    for item in number_input:
+        if item not in counted_numbers:
+            element_counter = number_input.count(item)
+
+            # Находим количетсво пар через сочетания
+            pair_counter += math.factorial(element_counter) \
+                // (2 * math.factorial(element_counter - 2))
+
+            counted_numbers.append(item)  # Убираем числа, для которых пары посчитаны
+
+    return pair_counter  # Количество пар элементов равных друг другу
+
+
+pair_counter_func()
---
+++ src/homework15/test_task3.py
@@ -0,0 +1,66 @@
+'''Tests for task3'''
+
+import unittest
+from unittest.case import TestCase
+from task3 import pair_counter_func
+from ddt import ddt, data, unpack
+
+
+@ddt
+class TestPairCounterFunc(TestCase):
+    '''Тесты для pair_counter_func()'''
+
+    @data(('1 1 1 1', 6),
+          ('1 1 1', 3),
+          (' 1 1 1 1 2 2', 7),
+          ('1 1 1 2 2 2 ', 6)
+          )
+    @ unpack
+    def test_positive_solo_numbers(self, value, expected):
+        '''Тесты, покрывают положительные одноразрядные числа'''
+
+        self.assertEqual(pair_counter_func(value), expected)
+
+    @data(1, None, True, [1, 2], (1, 2), {1, 2})
+    def test_wrong_input(self, value):
+        '''Тесты, рассчитанные на неверный тип входных данных'''
+
+        with self.assertRaises(AttributeError):
+            pair_counter_func(value)
+
+    @data(('-1 -1', 1),
+          ('-1 -1 -1', 3),
+          (' -1 -1 -1 -1 -2 -2 ', 7),
+          ('-1 -1 1 1 ', 2),
+          )
+    @unpack
+    def test_negative_numbers(self, value, expected):
+        '''Тесты, рассчитанные на наличие отрицательных чисел'''
+
+        self.assertEqual(pair_counter_func(value), expected)
+
+    @data(('1', 0),
+          ('1 1 1 2', 3),
+          (' 1 1 1 1 2 2 5', 7),
+          ('1 1 11', 1),
+          ('1 1 1 2 2 2 ', 6)
+          )
+    @ unpack
+    def test_no_pairs_numbers(self, value, expected):
+        '''Тесты, рассчитанные на наличие беспарных элементов'''
+
+        self.assertEqual(pair_counter_func(value), expected)
+
+    @data(('11 11', 1),
+          ('1 1 11 11 11', 4),
+          (' 111 111 22 22', 2)
+          )
+    @ unpack
+    def test_more_than_one_digit_numbers(self, value, expected):
+        '''Тесты, рассчитанные на числа имеющие более одного разряда'''
+
+        self.assertEqual(pair_counter_func(value), expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
