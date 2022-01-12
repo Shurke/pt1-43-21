39848845f847380c6aa1397219bfeb1ef5828b58
tkal <tkal@gurtam.com>
2022-01-12 23:35:08 +0300

    task1 test flake8 fix1

--- src/homework15/test_task1.py
+++ src/homework15/test_task1.py
@@ -7,24 +7,21 @@ import pytest
 
 
 def test_npc_choice_positive():
-    '''Test random npc choice in range
-    '''
+    '''Test random npc choice in range'''
 
     test_class = task1.Npc
     assert test_class.choice() in range(1, 4)
 
 
 def test_npc_choice_negative():
-    '''Test random npc choice in range
-    '''
+    '''Test random npc choice in range'''
 
     test_class = task1.Npc
     assert test_class.choice() in range(-1, 0)
 
 
 def test_npc_name_positive():
-    '''Test npc name in list
-    '''
+    '''Test npc name in list'''
 
     test_list = ['Шелдон', 'Пенни', 'Говард']
     test_class = task1.Npc
@@ -32,8 +29,7 @@ def test_npc_name_positive():
 
 
 def test_npc_name_negative():
-    '''Test npc name in list
-    '''
+    '''Test npc name in list'''
 
     test_list = ['A', 'B', 'C']
     test_class = task1.Npc
@@ -41,32 +37,28 @@ def test_npc_name_negative():
 
 
 def test_session_init_positive():
-    '''Test  positive init session class
-    '''
+    '''Test  positive init session class'''
 
     test_class = task1.Session(1, 3)
     assert test_class.win1 == 1 and test_class.win2 == 3
 
 
 def test_session_init_negative():
-    '''Test negative init session class
-    '''
+    '''Test negative init session class'''
 
     test_class = task1.Session(1, 3)
     assert test_class.win1 != 1 or test_class.win2 != 2
 
 
 def test_session_get_score_positive():
-    '''Test session.getscore
-    '''
+    '''Test session.getscore'''
 
     test_class = task1.Session(1, 3)
     assert test_class.get_score() == (1, 3)
 
 
 def test_session_get_score_negative():
-    '''Test session.getscore
-    '''
+    '''Test session.getscore'''
 
     test_class = task1.Session(1, 3)
     assert test_class.get_score() != (1, 5)
